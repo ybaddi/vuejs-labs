fa0b6a1026bdfb6143954d6a4c4abebed878bbb0
youssef baddi <baddiyoussef85@gmail.com>
2024-01-13 11:15:55 +0100

    add lab7

---
+++ Lab_7_v-model/script.js
@@ -0,0 +1,17 @@
+
+
+const application = {
+    data(){
+        return {
+            title: "Hello World",
+            message: "Hello",
+        }
+    },
+
+    methods:{
+       
+    }
+
+}
+
+Vue.createApp(application).mount("#app");
